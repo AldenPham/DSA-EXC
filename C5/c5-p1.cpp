2c6886871f2338820135e8af026477797e785d7f
PhamThanhTin1 <kawaiiidiein2007@gmail.com>
2026-09-09 23:41:32 +0700

    C5-P1 | added input processing

---
+++ C5/c5-p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, T; /* N: So luong san pham, T: Gia tri tong 2 sp */
+    cin >> N >> T;
+
+    int* a = new int[N];
+    for(int i = 0; i < N; i++){
+        cin >> a[i];
+    }
+
+}
